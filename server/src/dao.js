77962b06cec9a1feee45ccccfdb7c2f751da5753
Yoonho <jk16400@cau.ac.kr>
2023-01-04 00:47:41 +0900

    수정
    로그인 세션-로그아웃 추가, UI 통일, ejs로 파일 변환 (데이터 표시)

--- server/src/dao.js
+++ server/src/dao.js
@@ -3,12 +3,12 @@
 // 유저 생성
 async function insertUserInfo(connection, insertUserInfoParams) {
     const insertUserInfoQuery = `
-          INSERT INTO member(id, password, email, name, dept_name, point)
-          VALUES (?, ?, ?, ?, ?, ?);
-      `;
+        INSERT INTO member(id, password, email, name, dept_name, point)
+        VALUES (?, ?, ?, ?, ?, ?);
+    `;
     const insertUserInfoRow = await connection.query(
-      insertUserInfoQuery,
-      insertUserInfoParams
+        insertUserInfoQuery,
+        insertUserInfoParams
     );
     return insertUserInfoRow;
 }
@@ -16,9 +16,10 @@ async function insertUserInfo(connection, insertUserInfoParams) {
 // 로그인 : 유저 정보 조회
 async function selectUserInfo(connection, selectUserParams) {
     const selectUserQuery = `
-          SELECT id, password
-          FROM member
-          WHERE id = ? AND password = ?;`;
+        SELECT id, name, profile_url as profile, point
+        FROM member
+        WHERE id = ? AND password = ?;
+    `;
     const selectUserRow = await connection.query(
         selectUserQuery,
         selectUserParams
@@ -26,38 +27,58 @@ async function selectUserInfo(connection, selectUserParams) {
     return selectUserRow;
 }
 
-
-// 메인화면 조회
-async function selectMain(connection, userId){
-    const mainListQuery = `
-                  SELECT name, point
-                  FROM member
-                  WHERE id = '${userId}';
-                  `;
-    const [infoRows] = await connection.query(mainListQuery);
-    return infoRows;
+// 아이디로 회원 조회
+async function selectUserId(connection, userId) {
+    const selectUserIdQuery = `
+        SELECT id
+        FROM member
+        WHERE id = '${userId}';
+    `;
+    const [idRows] = await connection.query(selectUserIdQuery, userId);
+    return idRows;
 }
 
-// 내 과목 새 필드      ->      메인화면에 추가
-// 오늘의 인기 필드     ->      메인화면에 추가
 
+// 내 과목 새 필드 
+async function selectNewNote(connection, userId){
+    const selectNewNoteQuery = `
+        SELECT M2.name, C.course_name as course, N.explanation
+        FROM member M1, member M2, note N, interest I, course_detail C
+        WHERE M1.id = '${userId}' and I.member_id = M1.id and I.course_id = N.course_id and N.member_id = M2.id and I.course_id = C.course_id
+        ORDER BY upload_time desc;
+    `;
+    const [newNoteRows] = await connection.query(selectNewNoteQuery, userId);
+    return newNoteRows;
+}
+
+// 인기 필드     ->      메인화면에 추가
+async function selectPopularNote(connection){
+    const selectPopularNoteQuery = `
+        SELECT M.name, C.course_name as course, N.explanation
+        FROM note N, member M, course_detail C
+        WHERE N.member_id = M.id and N.course_id = C.course_id
+        ORDER BY hits desc;
+    `;
+    const [popularNoteRows] = await connection.query(selectPopularNoteQuery);
+    return popularNoteRows;
+}
 
 // 필기 정보 조회
-async function selectNote(connection, noteId){      // 필기 쓴 유저 이름, 과목명, 업로드 일자, 포인트, 설명
+async function selectNote(connection, noteId) {      // 필기 쓴 유저 이름, 과목명, 업로드 일자, 포인트, 설명
     const noteInfoQuery = `
-                  SELECT N.course_id, N.year, N.semester, N.instructor_name, N.upload_time, N.file_url, N.point, N.point, N.explanation, N.hits, 
+                  SELECT N.course_id, N.year, N.semester, N.instructor_name, N.upload_time, N.file_url, N.point, N.explanation, N.hits, 
                   M.name, C1.dept_name, C1.classification, C2.course_name
                   FROM note N natural join course C1, course_detail C2, member M
                   WHERE N.id = '${noteId}' and N.member_id = M.id and N.course_id = C2.course_id;
                   `;
     const [noteInfo] = await connection.query(noteInfoQuery);
-    return noteInfo;
+    return noteInfo[0];
 }
 
 // 리뷰 정보 조회
-async function selectReview(connection, noteId){
+async function selectReview(connection, noteId) {
     const reviewListQuery = `
-                  SELECT M.name, R.rating, R.comment, avg(rating)
+                  SELECT M.name, R.rating, R.comment
                   FROM note N, review R, member M
                   WHERE N.id = '${noteId}' and N.id = R.note_id and R.member_id = M.id;
                   `;
@@ -65,20 +86,39 @@ async function selectReview(connection, noteId){
     return reviewInfo;
 }
 
+// 과목 정보 조회
+async function selectCourse(connection) {
+    const courseListQuery = `
+                  SELECT course_name as name
+                  FROM course_detail;
+                  `;
+    const [courseInfo] = await connection.query(courseListQuery);
+    return courseInfo;
+}
+
 // 조회수 증가
-async function updateNoteHits(connection, noteId){
+async function updateNoteHits(connection, noteId) {
     const updateHitsQuery = `
                   UPDATE note
                   SET hits = hits + 1
                   WHERE id = '${noteId}';
                   `;
-    connection.query(updateHitsQuery, [noteId]);
+    const updateHits = connection.query(updateHitsQuery, [noteId]);
+    return updateHits[0];
 }
+
+
+// 필기 파일 추가       ->      field에서 post
+
 
 module.exports = {
     insertUserInfo,
     selectUserInfo,
-    selectMain,
+    selectUserId,
+    selectNewNote,
+    selectPopularNote,
+    selectCourse,
+
     selectNote,
     selectReview,
     updateNoteHits,
--- server/src/service.js
+++ server/src/service.js
@@ -3,18 +3,16 @@ const { pool } = require("../config/database");
 const provider = require("./provider");
 const dao = require("./dao");
 
-const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const {connect} = require("http2");
 
 
 exports.createUser = async function (id, pw, email, name, dept) {
     try {
-        /*              // 아이디 중복 확인 
-        const idRows = await userProvider.idCheck(ID);
+        // 아이디 중복 확인 
+        const idRows = await provider.idCheck(id);
         if(idRows.length > 0)
             return;
-        */
         
         // 비밀번호 암호화
         const hashedPassword = await crypto
@@ -37,14 +35,11 @@ exports.createUser = async function (id, pw, email, name, dept) {
 
 exports.postSignIn = async function (id, pw) {
     try {
-        /* 
-        // 이메일 여부 확인
-        const idRows = await userProvider.idCheck(ID);
-        if (idRows.length < 1) return errResponse(baseResponse.SIGNIN_ID_WRONG);
-
-        // const selectId = idRows[0].ID;
-        */
-
+        // 아이디 중복 확인 
+        const idRows = await provider.idCheck(id);
+        if(idRows.length < 1)
+            return;
+        
         // 비밀번호 확인
         const hashedPassword = await crypto
             .createHash("sha512")
@@ -55,33 +50,11 @@ exports.postSignIn = async function (id, pw) {
         const connection = await pool.getConnection(async (conn) => conn);
         const userInfoResult = await dao.selectUserInfo(connection, selectUserParams);
 
+        if(!userInfoResult[0])
+            return;
+
         return userInfoResult[0];
             
-        /*
-        const passwordRows = await userProvider.passwordCheck(selectUserPasswordParams);
-
-        if (passwordRows[0].password !== hashedPassword) {
-            return errResponse(baseResponse.SIGNIN_PASSWORD_WRONG);
-        }
-        // 계정 상태 확인
-        const userInfoRows = await userProvider.accountCheck(ID);
-
-        if(!userInfoRows[0])
-            return errResponse(baseResponse.SIGNIN_NO_ACCOUNT);
-        
-
-        //토큰 생성 Service
-        let token = await jwt.sign(
-            {
-                userId: id,
-            }, // 토큰의 내용(payload)
-            secret_config.jwtsecret, // 비밀키
-            {
-                expiresIn: "365d",
-                subject: "userInfo",
-            } // 유효 기간 365일
-        );
-        */
     } catch (err) {
         console.log(err);
     }
@@ -89,11 +62,12 @@ exports.postSignIn = async function (id, pw) {
 
 exports.updateHits = async function(noteId){
     try{
-        // + note 존재 여부 확인
         const connection = await pool.getConnection(async (conn) => conn);
-        await dao.updateNoteHits(connection, noteId);
+        const updateHitsResult = await dao.updateNoteHits(connection, noteId);
         connection.release();
 
+        return updateHitsResult;
+        
     } catch(err){
         console.log(err.massage);
     }
